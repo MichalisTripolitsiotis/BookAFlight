518f3f2740ee3878a280860f6bef7c3538f2fdcf
Michalis Tripolitsiotis <michalistripolitsiotis@gmail.com>
2022-06-25 13:10:22 +0300

    Create the form inputs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,18 +6,71 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <form>
-                <div class="relative z-0 w-full mb-6 group">
-                    <input type="text" name="from"
-                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                        placeholder=" " required />
-                    <label for="from"
-                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-                        From:
-                    </label>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-28">
+            <form action="/api/search" class="mt-3" method="POST">
+                @csrf
+                <div class="mb-6">
+                    <label class="block text-lg tracking-wide text-gray-500 dark:text-gray-400"
+                        for="from">From:</label>
+                    <input
+                        class="appearance-none block w-full rounded-md bg-white text-gray-700 border border-gray-200 py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="from" type="text">
                 </div>
-            </form>
+                <div class="mb-6">
+                    <label class="block text-lg tracking-wide text-gray-500 dark:text-gray-400"
+                        for="to">To:</label>
+                    <input
+                        class="appearance-none block w-full rounded-md bg-white text-gray-700 border border-gray-200 py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="to" type="text">
+                </div>
+                <div class="grid xl:grid-cols-2 xl:gap-6">
+                    <div class="relative z-0 w-full mb-6 group">
+                        <label class="block text-lg tracking-wide text-gray-500 dark:text-gray-400"
+                            for="departure-date">
+                            Departure Date:
+                        </label>
+                        <input
+                            class="appearance-none block w-full rounded-md bg-white text-gray-700 border border-gray-200 py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="departure-date" type="date">
+                    </div>
+                    <div class="relative z-0 w-full mb-6 group">
+                        <label class="block text-lg tracking-wide text-gray-500 dark:text-gray-400" for="return-date">
+                            Return Date:
+                        </label>
+                        <input
+                            class="appearance-none rounded-md block w-full bg-white text-gray-700 border border-gray-200  py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="return-date" type="date">
+                    </div>
+                </div>
+                <div class="grid xl:gap-1">
+                    <label class="block text-lg tracking-wide text-gray-500 dark:text-gray-400" for="adults">
+                        Adults (> 11 years old):
+                    </label>
+                    <input
+                        class="appearance-none rounded-md block w-full bg-white text-gray-700 border border-gray-200 py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="adults" type="number" value="1">
+
+                    <label class="block text-lg tracking-wide text-gray-500 dark:text-gray-400 mt-5" for="children">
+                        Children (2-11 years old):
+                    </label>
+                    <input
+                        class="appearance-none rounded-md block w-full bg-white text-gray-700 border border-gray-200  py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="children" type="number" value="0">
+
+                    <label class="block text-lg tracking-wide text-gray-500 dark:text-gray-400 mt-5" for="kids">
+                        Kids (< 2 years old): </label>
+                            <input
+                                class="appearance-none rounded-md block w-full bg-white text-gray-700 border border-gray-200 py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                id="kids" type="number" value="0">
+
+
+                            <button type="button"
+                                class="mt-10 text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+                                Search
+                            </button>
+                </div>
+
         </div>
+        </form>
     </div>
 </x-app-layout>
